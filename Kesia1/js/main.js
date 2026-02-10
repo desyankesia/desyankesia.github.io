132a0d8dae367ccafaa1847623a494a11bad19df
desyankesia <desto.yanto550@gmail.com>
2026-02-10 11:57:11 +0800

    Update main.js

--- Kesia1/js/main.js
+++ Kesia1/js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Valentine Days Sayanggkuu''I LOVE YOU').split('')
+    const titles = ('Happy Valentine Days Sayanggkuu').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
