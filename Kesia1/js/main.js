21b01e893c48f8edbbb2b98e2abc7e3bc37dcfc9
desyankesia <desto.yanto550@gmail.com>
2026-02-12 13:34:21 +0800

    Update main.js

--- Kesia1/js/main.js
+++ Kesia1/js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY VALENTINE DAYS').split('')
+    const titles = ('Happy Valentines Day').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
